08fabfc53704518466a0cffccec854283f2ddb2f
32bitx <131548833+32bitx@users.noreply.github.com>
2024-11-20 01:24:51 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
-let express = require( 'express' );
+let express = require('express');
 let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
-let path = require( 'path' );
-let favicon = require( 'serve-favicon' );
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon');
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
-} );
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
+// WebSocket stream setup
+io.of('/stream').on('connection', stream);
 
-io.of( '/stream' ).on( 'connection', stream );
-
-server.listen( 3000 );
+// Export serverless function for Vercel
+module.exports = (req, res) => {
+    server(req, res); // Vercel uses serverless functions, hence this line
+};
